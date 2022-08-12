996400ec913b7e8f1b4b7b8d5fce97a1b4f8c408
lvminh97 <lvminh97@gmail.com>
2022-08-12 23:12:49 +0700

    Update the GUI of a calculator
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

---
+++ TouchGFX/build/STM32F429IDISCO/TouchGFX/generated/images/src/__designer/image_Blue_Backgrounds_main_bg_portrait_240x320px.d
@@ -0,0 +1,5 @@
+TouchGFX/build/STM32F429IDISCO/TouchGFX/generated/images/src/__designer/image_Blue_Backgrounds_main_bg_portrait_240x320px.o: \
+ TouchGFX/generated/images/src/__designer/image_Blue_Backgrounds_main_bg_portrait_240x320px.cpp \
+ Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
